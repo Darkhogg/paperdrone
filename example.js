80c3325afecec737d7ea4d25a89435de36ac3dd8
Daniel Escoz <darkhogg@gmail.com>
2016-12-30 22:44:30 +0100

    Add queries plugin

--- example.js
+++ example.js
@@ -6,20 +6,22 @@ const sourceMaps = require('source-map-support');
 
 sourceMaps.install();
 
-var bot = pd.createBot(process.argv[3], {
+pd.createBot(process.argv[3], {
     'logger': bunyan.createLogger({
         'name': 'pd-ex-' + process.argv[2],
         'level': 'trace'
     })
-});
+}).then((bot) => {
 
-process.on('unhandledRejection', (err) => {
-    bot.logger.error(err);
-    process.exit(1);
-});
+    process.on('unhandledRejection', (err) => {
+        bot.logger.error(err);
+        process.exit(1);
+    });
 
-const ExamplePlugin = require('./dist/examples/' + process.argv[2]).default;
-bot.enable(new ExamplePlugin(), { 'mongo': 'mongodb://127.0.0.1/paperdrone', 'prefix': 'pd-ex.'+process.argv[2]+'.' }).then(() => {
-    bot.startPolling(3);
-    bot.startTicking(5);
+    const ExamplePlugin = require('./dist/examples/' + process.argv[2]).default;
+    bot.enable(new ExamplePlugin(), { 'mongo': 'mongodb://127.0.0.1/paperdrone', 'prefix': 'pd-ex.'+process.argv[2]+'.' }).then(() => {
+        bot.startPolling(3);
+        bot.startTicking(5);
+    });
+
 });
